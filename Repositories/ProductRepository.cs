a1a3e14d813ca75fc57b424ae3f9125631e2655d
EmiRing <emil.ringwall@gmail.com>
2021-07-16 09:28:06 +0200

    Added repositories

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using ArtWebshop.Data;
+using ArtWebshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtWebshop.Repositories
+{
+    public class ProductRepository : Repository<Product>
+    {
+        public ProductRepository(ProductDbContext context) :base(context)
+        {
+        }
+
+        public override Product Update(Product entity)
+        {
+            var product = _productContext.Products.Single(p => p.ProductId == entity.ProductId);
+
+            product.Title = entity.Title;
+            product.ArtistId = entity.ArtistId;
+            product.Width = entity.Width;
+            product.Height = entity.Height;
+
+            return base.Update(product);
+        }
+
+       
+
+
+    }
+}
---
+++ Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using ArtWebshop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ArtWebshop.Repositories
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected ProductDbContext _productContext;
+        
+        public Repository(ProductDbContext productDbContext)
+        {
+            _productContext = productDbContext;
+
+        }
+        public virtual T Add(T entity)
+        {
+            return _productContext.Add(entity).Entity;
+        }
+
+        public virtual IEnumerable<T> All()
+        {
+            return _productContext.Set<T>().ToList();
+        }
+
+        public virtual IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _productContext.Set<T>().AsQueryable().Where(predicate).ToList();
+        }
+
+        public virtual T Get(string id)
+        {
+            return _productContext.Find<T>(id);
+        }
+
+        public virtual void SaveChanges()
+        {
+            _productContext.SaveChanges();
+        }
+
+        public virtual T Update(T entity)
+        {
+            return _productContext.Update(entity).Entity;
+        }
+    }
+}
