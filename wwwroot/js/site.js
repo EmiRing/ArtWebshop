42f5f6998e6a311caf2d3d43fbec968947b49bc2
Mr-Lordmetal <lordmetal@msn.com>
2021-08-15 13:32:31 +0200

    Filter by Art, Artist & Price and sorteing each of them Ascending or Descendin and remeber the state is implemented. JQuery is moved to script.js,also implemented the buy notice on Info page (ProductInfo and a lot of other stuff is done

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,23 @@
-﻿
+﻿$(document).ready(function () {
+    $("#success-alert").hide();
+    $(".addToBasket").click(function showAlert() {
+        $("#success-alert").fadeTo(2000, 500).slideUp(500, function () {
+            $("#success-alert").slideUp(500);
+        });
+    });
+});
+
+
+$('#selectCriteria').change(function (event) {
+    var selectedCriteria = $('#selectCriteria').children("option:selected").val();
+    sessionStorage.setItem("itemCriteria", selectedCriteria);
+});
+
+$('select').find('option[value=' + sessionStorage.getItem('itemCriteria') + ']').attr('selected', 'selected');
+
+$('#selectAscDesc').change(function (event) {
+    var selectedAscDesc = $('#selectAscDesc').children("option:selected").val();
+    sessionStorage.setItem("itemAscDesc", selectedAscDesc);
+});
+
+$('select').find('option[value=' + sessionStorage.getItem('itemAscDesc') + ']').attr('selected', 'selected');
