d0aa2285174af843a0676e25e9e9ab3d4ceccc4d
Mr-Lordmetal <lordmetal@msn.com>
2021-08-18 16:57:25 +0200

    Fixed so the searchfilter is working again and removed a lot of old unecesary code  from ProductTemp

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,18 +6,3 @@
         });
     });
 });
-
-
-$('#selectCriteria').change(function (event) {
-    var selectedCriteria = $('#selectCriteria').children("option:selected").val();
-    sessionStorage.setItem("itemCriteria", selectedCriteria);
-});
-
-$('select').find('option[value=' + sessionStorage.getItem('itemCriteria') + ']').attr('selected', 'selected');
-
-$('#selectAscDesc').change(function (event) {
-    var selectedAscDesc = $('#selectAscDesc').children("option:selected").val();
-    sessionStorage.setItem("itemAscDesc", selectedAscDesc);
-});
-
-$('select').find('option[value=' + sessionStorage.getItem('itemAscDesc') + ']').attr('selected', 'selected');
