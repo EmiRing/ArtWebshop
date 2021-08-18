818f6dd1ecea6816d389e5c403e373237fbef7ec
Mr-Lordmetal <lordmetal@msn.com>
2021-08-18 17:09:04 +0200

    Changed JQuery script pasted from old branch and removed unecesary code from script file

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,9 @@
-﻿$(document).ready(function () {
+﻿var mouseX = 0;
+var mouseY = 0;
+$(document).ready(function () {
     $(".success-alert").hide();
-    $(".addToBasket").click(function showAlert() {
-        $(".success-alert").fadeTo(2000, 500).slideUp(500, function () {
+    $(".addToBasket").click(function showAlert(event) {
+        $(".success-alert").css({ 'left': event.pageX - 300, 'top': event.pageY + 20 }).fadeTo(2000, 500).slideUp(500, function () {
             $(".success-alert").slideUp(500);
         });
     });
